0426e250fa0b9b3d1fc01abd8dc180588b5d9b90
joonseolee <niceweather94@gmail.com>
2023-05-21 10:05:12 +0900

    feat: commit sync 예제 추가

--- consumers/src/main/kotlin/com/joonseolee/kafka/ConsumerCommit.kt
+++ consumers/src/main/kotlin/com/joonseolee/kafka/ConsumerCommit.kt
@@ -4,6 +4,7 @@ import mu.KotlinLogging
 import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.clients.consumer.KafkaConsumer
 import java.time.Duration
+import kotlin.math.log
 
 
 class ConsumerCommit : TestCallback {
@@ -12,7 +13,7 @@ class ConsumerCommit : TestCallback {
         val kafkaConsumer = KafkaConnector.generateKafkaConsumer {
             it.setProperty(ConsumerConfig.MAX_POLL_INTERVAL_MS_CONFIG, "60000")
             it.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "group_03")
-//            it.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "")
+            it.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false")
         }
         kafkaConsumer.subscribe(listOf(topicName))
 
@@ -28,7 +29,35 @@ class ConsumerCommit : TestCallback {
             }
         })
 
-        pollAutoCommit(kafkaConsumer)
+        // pollAutoCommit(kafkaConsumer)
+        pollCommitSync(kafkaConsumer)
+    }
+
+    private fun pollCommitSync(kafkaConsumer: KafkaConsumer<String, String>) {
+        var loopCount = 0
+        runCatching {
+            while (true) {
+                val consumerRecords = kafkaConsumer.poll(Duration.ofMillis(1000))
+                logger.info { ">>> looCount: ${loopCount++} consumerRecords count: ${consumerRecords.count()}" }
+                for (record in consumerRecords) {
+                    logger.info { "record key: ${record.key()}, partition: ${record.partition()}," +
+                            "record offset: ${record.offset()}, record value: ${record.value()}" }
+                }
+
+                runCatching {
+                    if (consumerRecords.count() < 1) {
+                        return
+                    }
+
+                    kafkaConsumer.commitSync()
+                    logger.info { "commit sync has been called" }
+                }.onFailure { logger.error { it } }
+            }
+        }.onFailure { logger.error { "error while polling $it" } }
+            .also {
+                logger.info { "get into the also step" }
+                kafkaConsumer.close()
+            }
     }
 
     private fun pollAutoCommit(kafkaConsumer: KafkaConsumer<String, String>) {
